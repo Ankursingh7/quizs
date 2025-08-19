0707329ce4c3693ca228fc589790e80e081be24e
Ankursingh7 <ankur.singh_cs.aiml24@gla.ac.in>
2025-08-19 11:56:24 +0530

    Update package.js

--- package.js
+++ package.js
@@ -11,7 +11,7 @@
   "author": "Your Name",
   "license": "MIT",
   "dependencies": {
-    "node-fetch": "^3.3.2"
+    "node-fetch": "^3.3.2",
     "pdf-parse": "^1.1.1"
   },
   "devDependencies": {
